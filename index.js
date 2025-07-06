995040301b045eb3fe254e460d515dd627d5677f
Abdul Rauf <itxrauf99@gmail.com>
2025-07-06 12:21:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+if (sessionStorage.getItem("visited")) {
+  var Random_No1 = Math.floor(Math.random() * 6 + 1);
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "./images/dice" + Random_No1 + ".png");
+  var Random_No2 = Math.floor(Math.random() * 6 + 1);
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "./images/dice" + Random_No2 + ".png");
+
+  if (Random_No1 == Random_No2) {
+    document.querySelector("h1").textContent = "Draw!";
+  } else if (Random_No1 > Random_No2) {
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!";
+  } else {
+    document.querySelector("h1").textContent = "🚩Player 2 Wins!";
+  }
+} else {
+  sessionStorage.setItem("visited", "true");
+}
